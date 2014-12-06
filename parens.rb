0b02418b2d4403c2dd673ed3d116e32c48446120
Matthias Flaig <matt.flaig@web.de>
2014-12-06 19:59:11 +0100

    remove deleted files

--- parens.rb
+++
@@ -1,7 +0,0 @@
-def get_closing_parens(str)
-  first_position = (str.index('('))+1
-  last_position = (str.rindex(')'))+1
-  return first_position, last_position
-end
-
-puts(get_closing_parens("Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing."))
