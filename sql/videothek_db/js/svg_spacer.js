729d784227a4e86655fb57fbb23a8aa13e704100
Matthias Flaig <matt.flaig@web.de>
2015-01-10 22:02:48 +0100

    random colours

--- sql/videothek_db/js/svg_spacer.js
+++ sql/videothek_db/js/svg_spacer.js
@@ -1,6 +1,5 @@
 (function($){
   $(document).ready(function(){
-    var counter = 0;
     initialize();
     setInterval(function(){ initialize() }, 15000);
 
@@ -10,27 +9,27 @@
     }
 
     function getSvgImage(){
-      var colours = ['red', 'blue', 'violet', 'yellow', 'green', 'maroon', 'teal', 'midnightblue', 'lime','brown','darkgreen','navy','black','indigo', 'saddlebrown'];
       var start_x = 0;
       var start_y = 0;
       var image = "<svg  width=100% height='300'>" ;
       for(var i = 0; i<7; i++ ){
         for(var j = 0; j<50; j++){
+
+          var randomRed = Math.floor(Math.random() * (256));
+          var randomGreen = Math.floor(Math.random() * (256));
+          var randomBlue = Math.floor(Math.random() * (256));
+
           image += "<rect x='" + start_x +"' y='" + start_y + 
-                   "' width='50' height='50' style='stroke-width:1;stroke:rgb(0,0,0);fill:" + colours[counter]+ ";'>" +"<animate attributeName='opacity'"+
+                   "' width='50' height='50' style='stroke-width:1;stroke:rgb(0,0,0);fill:rgb(" + 
+                   randomRed+ ","+ randomGreen + "," + randomBlue + ");'>" +"<animate attributeName='opacity'"+
                    "values='0.1;0.9;0.1' dur='15s'repeatCount='indefinite'/>" +"</rect>";
           start_x += 50;
-          if (counter == colours.length - 1){counter = 0;}
-          else{counter += 1;}
         }
         start_y += 50;
         start_x = 0;
       }
       image += "</svg>";
 
-      if (counter == colours.length - 1){counter = 0;}
-      else{counter += 1;}
-      
       return image;
     }
 
--- xml/edv_courses/js/svg_spacer.js
+++ xml/edv_courses/js/svg_spacer.js
@@ -1,6 +1,6 @@
 (function($){
   $(document).ready(function(){
-    var counter = 0;
+    // var counter = 0;
     initialize();
     setInterval(function(){ initialize() }, 15000);
 
@@ -10,14 +10,18 @@
     }
 
     function getSvgImage(){
-      var colours = ['teal','midnightblue','brown','darkgreen','navy','black','indigo', 'saddlebrown'];
+      // var colours = ['teal','midnightblue','brown','darkgreen','navy','black','indigo', 'saddlebrown'];
       var start_x = 0;
       var start_y = 0;
       var image = "<svg  width=100% height='300'>" ;
       for(var i = 0; i<5; i++ ){
         for(var j = 0; j<50; j++){
+          var randomRed = Math.floor(Math.random() * (256));
+          var randomGreen = Math.floor(Math.random() * (256));
+          var randomBlue = Math.floor(Math.random() * (256));
           image += "<circle cx='" + start_x +"' cy='" + start_y + 
-                   "' r='100' style='fill:" + colours[counter]+ ";'>" +"<animate attributeName='opacity'"+
+                   "' r='100' style='fill:rgb(" + 
+                   randomRed+ ","+ randomGreen + "," + randomBlue + ");'>" +"<animate attributeName='opacity'"+
                    "values='0.1;0.6;0.1' dur='15s'repeatCount='indefinite'/>" +"</circle>";
           start_x += 100;
         }
@@ -26,8 +30,8 @@
       }
       image += "</svg>";
 
-      if (counter == colours.length - 1){counter = 0;}
-      else{counter += 1;}
+      // if (counter == colours.length - 1){counter = 0;}
+      // else{counter += 1;}
       
       return image;
     }
