9daa96c8d8fa850dbf04ad11aed4942f5136074d
Matthias Flaig <matt.flaig@web.de>
2015-01-23 22:22:14 +0100

    rechner parsing

--- php/rechner/js/rechner.js
+++ php/rechner/js/rechner.js
@@ -1,161 +1,80 @@
 (function($){
 	$(document).ready(function(){
-	  var calc_string = "";
-      // var content = getCalculator();
-
-      // var calc_input = $('#calc_input').val();
-      // $('#calculator').html(content);
+	      var calc_string = "";
+            getCommaEvent();
 
 
-      $('#plus').click(function(event){
-      	event.preventDefault();
-            lastChar = calc_string.charAt(calc_string.length-1);
-      	if (lastChar != " "){
-      		calc_string += " + ";
-      		$('#calc_input').val(calc_string);
-      	}
-      	else{
-      		$('#calc_input').val("ERROR");
-      	}
-      });
-      $('#minus').click(function(event){
-      	event.preventDefault();
-            lastChar = calc_string.charAt(calc_string.length-1);
-            if (lastChar != " "){
-      		calc_string += " - ";
-      		$('#calc_input').val(calc_string);
-      	}
-      	else{
-      		$('#calc_input').val("ERROR");
-      	}
-      });
-      $('#mal').click(function(event){
-      	event.preventDefault();
-            lastChar = calc_string.charAt(calc_string.length-1);
-            if (lastChar != " "){
-      		calc_string += " * ";
-      		$('#calc_input').val(calc_string);
-      	}
-      	else{
-      		$('#calc_input').val("ERROR");
-      	}	
-      });
-      $('#geteilt').click(function(event){
-      	event.preventDefault();
-            lastChar = calc_string.charAt(calc_string.length-1);
-            if (lastChar != " "){
-      		calc_string += " / ";
-      		$('#calc_input').val(calc_string);
-      	}
-      	else{
-      		$('#calc_input').val("ERROR");
-      	}
-      });
-      $('#komma').click(function(event){
-            event.preventDefault();
-            lastChar = calc_string.charAt(calc_string.length-1);
-            if (lastChar != " "){
-                  calc_string += ".";
+            // Setting click events for operators and numbers
+            $('.button').each(function(index){
+                  $(this).on("click", function(){
+                        lastChar = calc_string.charAt(calc_string.length-1);
+
+                        // case for numbers  
+                        if(!isNaN($(this).attr("name"))){
+                              calc_string += $(this).attr("name"); 
+                        }
+
+                        // case for operators except comma
+                        else{
+                              if (lastChar && lastChar != " "){
+                                    calc_string += " " + $(this).attr("name") + " "; 
+                                    getCommaEvent();
+                              }
+                        }
+                        $('#calc_input').val(calc_string);
+                  });
+            });
+
+            //Clearing input
+            $('#loesche').click(function(event){
+                  calc_string = "";
                   $('#calc_input').val(calc_string);
+                  getCommaEvent();
+                  
+            });
+
+            // Submit event (Handling Ajax Post Request)
+            $('#submit').click(function(event){
+                  event.preventDefault();
+                  var request = postAjaxRequest();
+
+                  request.done(function(){
+                        var response = request.responseText;
+                        calc_string = parseFloat(response).toFixed(2);
+                        $('#calc_input').val(calc_string);
+                        removeCommaEvent();
+
+                  });
+            });
+
+            // Comma event functions
+            function getCommaEvent(){
+                  $('#comma').click(function(event){
+                        lastChar = calc_string.charAt(calc_string.length-1);
+                        if (lastChar && lastChar != " " && lastChar != "."){
+                              calc_string += ".";
+                              $('#calc_input').val(calc_string);
+                              removeCommaEvent();
+                        }
+                  });
             }
-            else{
-                  $('#calc_input').val("ERROR");
+
+            function removeCommaEvent(){
+                  $('#comma').off('click');
             }
-      });
-      $('#loesche').click(function(event){
-            event.preventDefault();
-            calc_string = "";
-            $('#calc_input').val(calc_string);
+
             
-      });
-      $('#modulo').click(function(event){
-            event.preventDefault();
-            lastChar = calc_string.charAt(calc_string.length-1);
-            if (lastChar != " "){
-                  calc_string += " % ";
-                  $('#calc_input').val(calc_string);
+
+            // Ajax Post Request
+            function postAjaxRequest(){
+                  var request = $.ajax({
+                    type: "POST",
+                    url: "php/rechner.php",
+                    data: $('form').serialize()
+                    
+                  });
+                  return request;
             }
-            else{
-                  $('#calc_input').val("ERROR");
-            }
-      });
-
-
-
-
-
-      $('#eins').click(function(event){
-      	event.preventDefault();
-      	calc_string += "1";
-      	$('#calc_input').val(calc_string);
-      });
-      $('#zwei').click(function(event){
-      	event.preventDefault();
-      	calc_string += "2";
-      	$('#calc_input').val(calc_string);
-      });
-      $('#drei').click(function(event){
-      	event.preventDefault();
-      	calc_string += "3";
-      	$('#calc_input').val(calc_string);
-      });
-      $('#vier').click(function(event){
-      	event.preventDefault();
-      	calc_string += "4";
-      	$('#calc_input').val(calc_string);
-      });
-      $('#fuenf').click(function(event){
-      	event.preventDefault();
-      	calc_string += "5";
-      	$('#calc_input').val(calc_string);
-      });
-      $('#sechs').click(function(event){
-      	event.preventDefault();
-      	calc_string += "6";
-      	$('#calc_input').val(calc_string);
-      });
-      $('#sieben').click(function(event){
-      	event.preventDefault();
-      	calc_string += "7";
-      	$('#calc_input').val(calc_string);
-      });
-      $('#acht').click(function(event){
-      	event.preventDefault();
-      	calc_string += "8";
-      	$('#calc_input').val(calc_string);
-      });
-      $('#neun').click(function(event){
-            event.preventDefault();
-            calc_string += "9";
-            $('#calc_input').val(calc_string);
-      });
-      $('#null').click(function(event){
-            event.preventDefault();
-            calc_string += "0";
-            $('#calc_input').val(calc_string);
-      });
-
-
-      $('#submit').click(function(event){
-            event.preventDefault();
-            var request = postAjaxRequest();
-
-            request.done(function(){
-              var response = request.responseText;
-              $('#calc_input').val(response);
-            });
-      });
-
-      function postAjaxRequest(){
-            var request = $.ajax({
-              type: "POST",
-              url: "php/rechner.php",
-              data: $('form').serialize()
-              
-            });
-            return request;
-      }
-
 
 	});
 
--- php/rechner/php/rechner.php
+++ php/rechner/php/rechner.php
@@ -1,8 +1,76 @@
 <?php
-  $expression = $_POST['calc_input'];
-  $result = eval(" return ($expression);");
-  echo $result;
+  
+	if ($_POST['calc_input'] != ""){
+		$expression = $_POST['calc_input'];
+		$arr = explode(" ", $expression);
+		$sum = 0;
+
+		if (preg_match("/\/\s0/", $expression)){
+            echo "ERROR";
+        }
+        else{
+		    function evaluate($arr, $sum){   
+		    
+				if (!function_exists('parse')){
+				    function parse($arr, $operator){
+					    for ($i=0;$i<count($arr);$i++){
+							$element = $arr[$i];
+
+							if ($element == $operator){
+								switch ($element){ 
+									case '*': $new_element = floatval($arr[$i-1]) * floatval($arr[$i+1]);
+										break;
+									case '/': $new_element = floatval($arr[$i-1]) / floatval($arr[$i+1]);
+										break;
+									case '%': $new_element = floatval($arr[$i-1]) % floatval($arr[$i+1]);
+										break;
+									case '-': $new_element = floatval($arr[$i-1]) - floatval($arr[$i+1]);
+										break;
+									case '+': $new_element = floatval($arr[$i-1]) + floatval($arr[$i+1]);
+										break;
+								}
+
+								if (count($arr) > 3){
+									array_splice($arr, $i-1, 3, $new_element);
+									$i=0;
+								}
+								else{
+									$GLOBALS['sum'] += $new_element;
+								}
+							}	
+					    }
+					    return $arr;
+				    }
+				}
+
+				$operators = array('*','/','%','-','+');
+				$index = 0;
+
+				while ($index < count($operators)){
+					$parsed_array = parse($arr, $operators[$index]);
+					$index++;
+					$arr = $parsed_array;
+				} 
+
+				return $parsed_array;
+
+			    // $multiplied = parse($arr, '*');
+			    // $divided = parse($multiplied, '/');
+			    // $moduloed = parse($divided, '%');
+			    // $subtracted = parse($moduloed, '-');
+			    // $added = parse($subtracted, '+');
+	      //       return $added;
+			}
+
+			evaluate($arr, $sum);
+			echo round($sum, 2);
+			
+		}
+
+	}
+	else{
+		echo "";
+	}
 
 
-  //explode und implode verwenden
 ?>
