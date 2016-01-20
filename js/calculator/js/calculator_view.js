27db346e5c2377427315893000f7198f84bb5a82
Matthias Flaig <matt.flaig@web.de>
2016-01-20 15:24:25 +0100

    added first event handler

--- js/calculator/js/calculator_view.js
+++ js/calculator/js/calculator_view.js
@@ -1,11 +1,25 @@
 var view = function(){
 
 	var _setup = function(){
-		var input = model.get_rows();
+		view.load_calculator();
+		view.load_event_handler();
+	}
+
+	var _load_event_handler = function(){
+		$('#calculator').on('click', '.calc', function(){
+			console.log($(this).attr('name'));
+			var input = $(this).attr('name');
+			var old_input = $('#calc_input').val();
+			$('#calc_input').val(old_input + input);
+		})
+	}
+
+	var _load_calculator = function(){
+        var rows = model.get_rows();
 		var html = "";
 
 		html += view.build_first_row();
-		html += view.build_main_rows(input);
+		html += view.build_main_rows(rows);
 		html += view.build_last_row();
 		
 		$('#calculator').html(html);
@@ -17,12 +31,12 @@ var view = function(){
                 "</row>";
 	}
 
-	var _build_main_rows = function(input){
+	var _build_main_rows = function(rows){
 		var html = "";
         for(var i = 0; i < 4; i++){
 			html += "<row>";
-			input[i].forEach(function(element){
-				html += "<div class='button col-sm-3' name=" + "'" + element + "'" + ">" + element +"</div>";
+			rows[i].forEach(function(element){
+				html += "<div class='calc button col-sm-3' name=" + "'" + element + "'" + ">" + element +"</div>";
 			});
 			html += "</row>";
 		}
@@ -43,7 +57,9 @@ var view = function(){
 		setup : _setup,
 		build_first_row : _build_first_row,
 		build_main_rows : _build_main_rows,
-		build_last_row : _build_last_row
+		build_last_row : _build_last_row,
+		load_calculator : _load_calculator,
+		load_event_handler : _load_event_handler
 	}
 
 	
